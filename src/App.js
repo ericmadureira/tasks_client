3f4d16f0b6c79ca7006251eb6f9522365a0b2dc1
ericmadureira <ericmadureira.uesc@gmail.com>
2019-05-13 01:48:08 -0300

    add TaskList component and basic scss

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.scss';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import './App.scss';
+
+import Container from 'react-bootstrap/Container'
+import TaskList from './components/TaskList';
 import Header from './components/Header';
    
 library.add(faCheckCircle, faTrashAlt)
@@ -11,6 +15,9 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <Container>
+        <TaskList/>
+      </Container>
     </div>
   );
 }
