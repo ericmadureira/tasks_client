411201748d3f14982777937d95f73f71ee069a7b
ericmadureira <ericmadureira.uesc@gmail.com>
2019-05-21 20:50:04 -0300

    make the react client consume the /tasks endpoint

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -24,11 +24,11 @@ class TaskList extends Component {
 			<Row>
 				<Col xs={{ span: 8, offset: 2 }} className="tasks_list">
 					<p className="title">To-do</p>
-					<TasksCard tasks={[{'title': 'Criar Header Criar Header Criar Header', 'done': false}, {'title': 'Criar footer', 'done': false}, {'title': 'Criar footer', 'done': false}]} />
+					<TasksCard tasks={this.state.tasks.filter((task) => task.done != true)} />
 				</Col>
 				<Col xs={{ span: 8, offset: 2 }} className="tasks_list">
 					<p className="title">Done</p>
-					<TasksCard tasks={[{'title': 'Criar Header Criar Header Criar Header', 'done': true}, {'title': 'Criar footer', 'done': true}, {'title': 'Criar footer', 'done': true}]} />
+					<TasksCard tasks={this.state.tasks.filter((task) => task.done == true)} />
 				</Col>
 			</Row>
 		);
