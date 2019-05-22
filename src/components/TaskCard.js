c9c82d9833365ed22b52d5e95244389f195ff2b2
ericmadureira <ericmadureira.uesc@gmail.com>
2019-05-21 23:26:51 -0300

    allow to check tasks as done through EDIT endpoint

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -13,6 +13,20 @@ class TaskCard extends Component {
     }
   }
 
+  async checkTask(task) {
+    let form = {'task': {'done': 'true'}}
+    await fetch(`http://localhost:3001/tasks/${task.id}`,
+    {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        task: { done: true }})
+    });
+    this.props.loadTasks();
+  }
+
   render() {
     return (
       <div>
@@ -24,7 +38,7 @@ class TaskCard extends Component {
                   return <tr key={task.id}>
                     <td className="col-md-10">{task.title}</td>
                     <td>
-                      { task.done == false ? <a className="check" href="#"><FontAwesomeIcon icon="check-circle"/></a> : null }
+                      { task.done == false ? <a className="check" onClick={() => this.checkTask(task)} href="#"><FontAwesomeIcon icon="check-circle"/></a> : null }
                     </td>
                     <td>
                       <a className="delete" onClick={() => this.deleteTask(task)} href="#">
