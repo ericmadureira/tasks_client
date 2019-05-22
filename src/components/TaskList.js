b8cbd72ae7099f8741c461028be6d898c19c6823
ericmadureira <ericmadureira.uesc@gmail.com>
2019-05-22 00:58:38 -0300

    create tasks through CREATE endpoint

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+import CreateTask from './CreateTasks';
 import TaskCard from './TaskCard';
 
 class TaskList extends Component {
@@ -26,10 +29,13 @@ class TaskList extends Component {
 				<Col xs={{ span: 8, offset: 2 }} className="tasks_list">
 					<p className="title">To-do</p>
 					<TaskCard loadTasks={this.loadTasks} tasks={this.state.tasks.filter((task) => task.done != true)} />
+					<CreateTask loadTasks={this.loadTasks}/>
 				</Col>
 				<Col xs={{ span: 8, offset: 2 }} className="tasks_list">
 					<p className="title">Done</p>
 					<TaskCard loadTasks={this.loadTasks} tasks={this.state.tasks.filter((task) => task.done == true)} />
+					<br/>
+					<Button variant="dark" className="float-right remove_tasks_btn">Remove all tasks</Button>					
 				</Col>
 			</Row>
 		);
